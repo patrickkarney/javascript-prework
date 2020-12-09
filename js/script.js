a14d58b0fed0c60065fdfe72bd73cee31d91a6b4
Patrick Karney <patrickkarney@gmail.com>
2020-12-09 11:14:20 +0100

    Change variables to const

--- js/script.js
+++ js/script.js
@@ -48,15 +48,15 @@ function playGame(playerInput){
         
     clearMessages()
 
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
 
     console.log('Wylosowana liczba to: ' + randomNumber);
 
-    let computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
 
     console.log('Gracz wpisał: ' + playerInput);
 
-    let playerMove = getMoveName(playerInput);    
+    const playerMove = getMoveName(playerInput);    
 
     score(displayResult(computerMove, playerMove));
 
